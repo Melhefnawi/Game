593c719441faa02a45182012ff115ee75013519c
Melhefnawi <>
2023-04-23 07:55:22 +0000

    adding the Event Listers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+//wait for the DOM to finish loading before running the game
+// get the button element and add event listerner to them 
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("you clicked submit!");
+
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`you clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+
+
+
 function runGame() {
 
 }
