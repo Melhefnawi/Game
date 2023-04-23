50d1d485f75e1541335c07af2a3761c023b60c13
Melhefnawi <>
2023-04-23 09:17:40 +0000

    building the displayaddition function

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,9 +20,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionalQuestion(num1, num2);
+    } else {
+        alert(`unknow game type:${gameType}`);
+        throw `unknow gameType:${gameType}.aborting!`;
+    }
 
 }
 function checkAnswer() {
@@ -37,9 +44,10 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
-function displayAdditionalQuestion(operand1,operand2) {
-document.getElementById("operand1").textContent=operand1;
-document.getElementById("operand2").textContent=operand2;
+function displayAdditionalQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 function displaySubtractQuestion() {
